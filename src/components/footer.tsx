fa6e8c1c2fc4a6b8fd4b1ecd27a8c92946f12c6e
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2024-09-08 02:29:33 +0530

    Add Footer and Header components

---
+++ src/components/footer.tsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/joy";
+import React from "react";
+import Link from "@mui/joy/Link";
+
+export default function Footer() {
+    return (
+        <Typography
+            component="p"
+            level="title-md"
+            mt='auto'
+            mb={5}
+            sx={{
+                color: 'white'
+            }}
+            textAlign='center'
+        >
+            &copy; 2023+ <Link href="https://github.com/YasogaN/">YasogaN</Link> | Powered by Cloudflare Pages and Google Programmable Search Engine | <Link href="https://github.com/YasogaN/APK-Mods-Search">GitHub</Link> | <Link href="https://github.com/YasogaN/APK-Mods-Search/blob/v2/TERMS_OF_USE.md">Terms of Use</Link> | <Link href="https://github.com/YasogaN/APK-Mods-Search/blob/v2/PRIVACY_POLICY.md">Privacy Policy</Link> | <Link href="https://github.com/YasogaN/APK-Mods-Search/blob/v2/SOURCES.md">Sources List</Link>
+        </Typography>
+    );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Card from '@mui/joy/Card';
+import Typography from '@mui/joy/Typography';
+
+export default function Header() {
+    return (
+        <>
+            <Card
+                variant="soft"
+                sx={{
+                    backgroundColor: '#9df4ff',
+                    pointerEvents: 'none',
+                    userSelect: 'none',
+                    marginTop: '4vh',
+                    marginBottom: '2vh',
+                }}>
+                <img
+                    src="./logo.png"
+                    loading="lazy"
+                    alt=""
+                />
+            </Card>
+            <Typography
+                component="h1"
+                level="h1"
+                m={2}
+                sx={{
+                    color: 'white'
+                }}
+                textAlign='center'
+            >
+                APK Mods Search
+            </Typography>
+            <Typography
+                component="p"
+                level="body-md"
+                m={2}
+                sx={{
+                    color: 'white'
+                }}
+                textAlign='center'
+            >
+                Made with ❤️ by YasogaN
+            </Typography>
+        </>
+    );
+}
