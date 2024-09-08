76d3728a24e06d05cb53c0c6f3a5e6e0f47dc00c
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2024-09-08 17:50:57 +0530

    Add ResultCard component

---
+++ src/components/card.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import Typography from '@mui/joy/Typography';
+import Link from '@mui/joy/Link';
+import Stack from '@mui/joy/Stack';
+
+export default function ResultCard({ site, title, description, link, imageUrl }: { site: string, title: string, description: string, link: string, imageUrl: string }) {
+    return (
+        <Link href={link} target="_blank">
+            <Card orientation="horizontal" variant="soft" sx={{ width: '75vw', margin: 2, backgroundColor: '#1c2020', maxHeigh: 120 }}>
+                <CardContent>
+                    <Stack
+                        direction="row"
+                        spacing={2}
+                        sx={{
+                            justifyContent: "flex-start",
+                            alignItems: "center",
+                        }}
+                    >
+                        <AspectRatio ratio="1" sx={{ width: 50, borderRadius: '10px', }}>
+                            <img
+                                src={imageUrl}
+                                loading="lazy"
+                            />
+                        </AspectRatio>
+                        <Stack
+                            direction="column"
+                            spacing={0}
+                            sx={{
+                                justifyContent: "flex-start",
+                                alignItems: "flex-start",
+                            }}
+                        >
+                            <Typography textColor="primary.plainColor" level='title-md'>
+                                {title}
+                            </Typography>
+                            <Typography level="body-sm">
+                                {site}
+                            </Typography>
+                            <Typography level="body-xs">
+                                {description}
+                            </Typography>
+                        </Stack>
+                    </Stack>
+                </CardContent>
+            </Card>
+        </Link>
+    );
+}
