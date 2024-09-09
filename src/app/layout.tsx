473473d990a0aa353839960c6d4cf9dfed83fa96
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2024-09-09 20:50:21 +0530

    Update metadata, add Open Graph tags in layout.tsx and add image for OG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,23 @@ import * as React from 'react';
 import ThemeRegistry from '@/components/ThemeRegistry/ThemeRegistry';
 
 export const metadata = {
-  title: 'APK Mods Search',
-  description: 'A Search engine to search for APK Mods',
+  title: 'APK Mods Search | Find Modded APKs Fast',
+  description: 'APK Mods Search is a fast and reliable search engine for discovering modded APKs from trusted sources. Powered by Cloudflare and Google CSE, get the best APK mods quickly.',
 };
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="darkreader-lock" />
+        <meta property="og:title" content="APK Mods Search | Find Modded APKs Fast" />
+        <meta property="og:description" content="APK Mods Search is a fast and reliable search engine for discovering modded APKs from trusted sources. Powered by Cloudflare and Google CSE, get the best APK mods quickly." />
+        <meta property="og:image" content="https://apkmodsearch.pages.dev/ogimage.jpg" />
+        <meta property="og:url" content="https://apkmodsearch.pages.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="APK Mods Search" />
+      </head>
       <body>
         <ThemeRegistry>{props.children}</ThemeRegistry>
       </body>
